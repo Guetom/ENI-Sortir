46195256f0c1d3dc5f2d94af169c9f78c41c5cbc
TheMisterRedFox <93584901+TheMisterRedFox@users.noreply.github.com>
2024-02-01 12:05:26 +0100

    feat: add image upload when hover image

--- public/js/dynamicPicture.js
+++ public/js/dynamicPicture.js
@@ -1,17 +1,20 @@
-const previewHandler = document.querySelector('.dynamic-picture-handler');
+const previewHandler = document.querySelector('input[type=file].dynamic-picture-handler');
 const dynamicPicture = document.querySelector('.dynamic-picture');
 
-if(previewHandler && dynamicPicture) {
-    previewHandler.addEventListener('change', function() {
+if (previewHandler && dynamicPicture) {
+    previewHandler.addEventListener('change', function () {
         previewImage(this);
     });
 
-    function previewImage(input) {
+    dynamicPicture.addEventListener('click', function () {
+        previewHandler.click();
+    });
 
+    function previewImage(input) {
         if (input.files && input.files[0]) {
             const reader = new FileReader();
 
-            reader.onload = function(e) {
+            reader.onload = function (e) {
                 dynamicPicture.src = e.target.result;
                 dynamicPicture.style.display = 'block';
             };
