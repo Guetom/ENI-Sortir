f916949aec3b4250a3136bc019eeb6e739d71798
Guetom <thomascacatoes@gmail.com>
2024-02-06 09:52:21 +0100

    Change function to check email

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -65,9 +65,9 @@ class ResetPasswordController extends AbstractController
             $resetToken = $this->resetPasswordHelper->generateFakeResetToken();
         }
 
-        return $this->render('reset_password/check_email.html.twig', [
-            'resetToken' => $resetToken,
-        ]);
+        $this->addFlash('reset_password', 'Un email vous a été envoyé pour réinitialiser votre mot de passe. Merci de vérifier votre boîte mail.');
+
+        return $this->redirectToRoute('app_login');
     }
 
     /**
