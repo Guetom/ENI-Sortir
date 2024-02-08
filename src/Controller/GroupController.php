7a9476549e14dfeaa6312cc48edc462acef1eb05
Guetom <thomascacatoes@gmail.com>
2024-02-08 13:45:17 +0100

    Create index page group

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\GroupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/group', name: 'group_')]
 class GroupController extends AbstractController
 {
-    #[Route('/group', name: 'app_group')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(GroupRepository $groupRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
         return $this->render('group/index.html.twig', [
-            'controller_name' => 'GroupController',
+            'groups' => $groupRepository->findMyGroup($this->getUser()),
         ]);
     }
 }
