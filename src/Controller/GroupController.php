7f4b3d046ea86a49ef6dfe1ee00e10dfe4620a46
Guetom <thomascacatoes@gmail.com>
2024-02-08 11:54:52 +0100

    Create group controller

---
+++ src/Controller/GroupController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GroupController extends AbstractController
+{
+    #[Route('/group', name: 'app_group')]
+    public function index(): Response
+    {
+        return $this->render('group/index.html.twig', [
+            'controller_name' => 'GroupController',
+        ]);
+    }
+}
